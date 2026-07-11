bca31db6101ab1a8bfbdc24ed1fd3f80db40d239
Fyrgeit <mans@peronilla.se>
2026-07-11 22:23:46 +0200

    Included dist (js files)

---
+++ dist/index.js
@@ -0,0 +1,309 @@
+import { FormatName, FindPerson, FormatFamily, AddPerson, FindFamily, AddFamily, download, open, clear, } from './typesnmethods.js';
+function refreshLists() {
+    peopleSection.innerHTML = '';
+    familiesSection.innerHTML = '';
+    localStorage.setItem('openedFile', JSON.stringify(openedFile));
+    if (!openedFile)
+        return;
+    openedFile.people.forEach((p) => {
+        let el = document.createElement('p');
+        el.innerHTML = FormatName(p, 'extra') ?? '?';
+        if (p.id === selectedPerson)
+            el.classList.add('embolden');
+        el.setAttribute('data-id', '' + p.id);
+        el.setAttribute('data-type', 'person');
+        el.onclick = select;
+        peopleSection.append(el);
+    });
+    openedFile.families.forEach((f) => {
+        let el = document.createElement('p');
+        el.innerHTML = FormatFamily(openedFile, f);
+        if (f.id === selectedFamily)
+            el.classList.add('embolden');
+        el.setAttribute('data-id', '' + f.id);
+        el.setAttribute('data-type', 'family');
+        el.onclick = select;
+        familiesSection.append(el);
+    });
+}
+function refreshPersonInspector() {
+    localStorage.setItem('selectedPerson', selectedPerson !== null ? '' + selectedPerson : 'null');
+    personInspector.innerHTML = '';
+    if (selectedPerson === null) {
+        personInspector.innerHTML = 'Välj person';
+        return;
+    }
+    let keys = [
+        'nameFirst',
+        'nameLast',
+        'nameLastMaiden',
+        'dateBirth',
+        'dateDeath',
+    ];
+    let person = FindPerson(openedFile, selectedPerson);
+    let formEl = document.createElement('form');
+    formEl.classList.add('person');
+    formEl.onchange = (e) => {
+        let target = e.target;
+        if (!target)
+            return;
+        let key = target.id;
+        //@ts-ignore
+        if (!key || !keys.includes(key))
+            return;
+        let k = key;
+        person[k] = target.value;
+        refreshLists();
+    };
+    let titleEl = document.createElement('p');
+    titleEl.innerHTML = '#' + person.id;
+    titleEl.style.gridColumnEnd = 'span 2';
+    formEl.append(titleEl);
+    keys.forEach((key) => {
+        let labelEl = document.createElement('label');
+        labelEl.innerHTML = key;
+        labelEl.setAttribute('for', key);
+        let inputEl = document.createElement('input');
+        inputEl.setAttribute('type', 'text');
+        inputEl.id = key;
+        inputEl.value = '' + person[key];
+        formEl.append(labelEl);
+        formEl.append(inputEl);
+    });
+    personInspector.append(formEl);
+    let inpEl = document.getElementById('nameFirst');
+    inpEl?.focus();
+}
+function refreshFamilyInspector() {
+    localStorage.setItem('selectedFamily', selectedFamily !== null ? '' + selectedFamily : 'null');
+    familyInspector.innerHTML = '';
+    if (selectedFamily === null) {
+        familyInspector.innerHTML = 'Välj familj';
+        return;
+    }
+    let keys = ['nameLastOverride', 'dateStart'];
+    let family = FindFamily(openedFile, selectedFamily);
+    let formEl = document.createElement('form');
+    formEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+    formEl.classList.add('family');
+    formEl.onchange = (e) => {
+        let target = e.target;
+        if (!target)
+            return;
+        let key = target.id;
+        //@ts-ignore
+        if (!key || !keys.includes(key))
+            return;
+        let k = key;
+        family[k] = target.value;
+        refreshLists();
+    };
+    let titleEl = document.createElement('p');
+    titleEl.innerHTML = '#' + family.id;
+    titleEl.style.gridColumnEnd = 'span 2';
+    formEl.append(titleEl);
+    let husbandLabel = document.createElement('label');
+    husbandLabel.innerHTML = 'Make';
+    formEl.append(husbandLabel);
+    let husbandContainer = document.createElement('div');
+    husbandContainer.classList.add('hcont');
+    let husbandEl = document.createElement('p');
+    husbandEl.classList.add('grow');
+    husbandEl.innerHTML = family.husband
+        ? FormatName(FindPerson(openedFile, family.husband), 'full')
+        : '?';
+    husbandContainer.append(husbandEl);
+    if (family.husband === null) {
+        let addHusband = document.createElement('button');
+        addHusband.innerHTML = '➕';
+        addHusband.setAttribute('type', 'button');
+        addHusband.onclick = () => {
+            family.husband = selectedPerson;
+            refreshLists();
+            refreshFamilyInspector();
+        };
+        husbandContainer.append(addHusband);
+    }
+    else {
+        let removeHusband = document.createElement('button');
+        removeHusband.innerHTML = '➖';
+        removeHusband.setAttribute('type', 'button');
+        removeHusband.onclick = () => {
+            family.husband = null;
+            refreshLists();
+            refreshFamilyInspector();
+        };
+        husbandContainer.append(removeHusband);
+    }
+    formEl.append(husbandContainer);
+    let wifeLabel = document.createElement('label');
+    wifeLabel.innerHTML = 'Maka';
+    formEl.append(wifeLabel);
+    let wifeContainer = document.createElement('div');
+    wifeContainer.classList.add('hcont');
+    let wifeEl = document.createElement('p');
+    wifeEl.classList.add('grow');
+    wifeEl.innerHTML = family.wife
+        ? FormatName(FindPerson(openedFile, family.wife), 'full')
+        : '?';
+    wifeContainer.append(wifeEl);
+    if (family.wife === null) {
+        let addWife = document.createElement('button');
+        addWife.innerHTML = '➕';
+        addWife.setAttribute('type', 'button');
+        addWife.onclick = () => {
+            family.wife = selectedPerson;
+            refreshLists();
+            refreshFamilyInspector();
+        };
+        wifeContainer.append(addWife);
+    }
+    else {
+        let removeWife = document.createElement('button');
+        removeWife.innerHTML = '➖';
+        removeWife.setAttribute('type', 'button');
+        removeWife.onclick = () => {
+            family.wife = null;
+            refreshLists();
+            refreshFamilyInspector();
+        };
+        wifeContainer.append(removeWife);
+    }
+    formEl.append(wifeContainer);
+    let childrenLabel = document.createElement('label');
+    childrenLabel.innerHTML = 'Barn';
+    formEl.append(childrenLabel);
+    let childrenContainer = document.createElement('div');
+    childrenContainer.classList.add('children');
+    family.children.forEach((c) => {
+        let childContainer = document.createElement('div');
+        childContainer.classList.add('hcont');
+        let child = FindPerson(openedFile, c);
+        let childEl = document.createElement('p');
+        childEl.innerHTML = FormatName(child, 'full');
+        childEl.classList.add('grow');
+        childContainer.append(childEl);
+        let arrowsEl = document.createElement('div');
+        arrowsEl.classList.add('arrows');
+        let up = document.createElement('button');
+        up.innerHTML = '🔼';
+        let down = document.createElement('button');
+        down.innerHTML = '🔽';
+        arrowsEl.append(up);
+        arrowsEl.append(down);
+        childContainer.append(arrowsEl);
+        let removeChild = document.createElement('button');
+        removeChild.innerHTML = '➖';
+        removeChild.setAttribute('type', 'button');
+        removeChild.onclick = () => {
+            family.children = family.children.filter((fc) => fc !== c);
+            refreshLists();
+            refreshFamilyInspector();
+        };
+        childContainer.append(removeChild);
+        childrenContainer.append(childContainer);
+    });
+    let addChild = document.createElement('button');
+    addChild.innerHTML = '➕';
+    addChild.setAttribute('type', 'button');
+    addChild.onclick = () => {
+        if (selectedPerson == null || family.children.includes(selectedPerson))
+            return;
+        family.children.push(selectedPerson);
+        refreshLists();
+        refreshFamilyInspector();
+    };
+    childrenContainer.append(addChild);
+    formEl.append(childrenContainer);
+    keys.forEach((key) => {
+        let labelEl = document.createElement('label');
+        labelEl.innerHTML = key;
+        labelEl.setAttribute('for', key);
+        let inputEl = document.createElement('input');
+        inputEl.setAttribute('type', 'text');
+        inputEl.id = key;
+        inputEl.value = '' + family[key];
+        formEl.append(labelEl);
+        formEl.append(inputEl);
+    });
+    familyInspector.append(formEl);
+}
+function select(e) {
+    let target = e.target;
+    let id = target.attributes.getNamedItem('data-id')?.value;
+    let type = target.attributes.getNamedItem('data-type')?.value;
+    if (type === 'person') {
+        if (id === undefined) {
+            selectedPerson = null;
+            refreshPersonInspector();
+            return;
+        }
+        selectedPerson = parseInt(id);
+        refreshPersonInspector();
+    }
+    else if (type === 'family') {
+        if (id === undefined) {
+            selectedFamily = null;
+            refreshFamilyInspector();
+            return;
+        }
+        selectedFamily = parseInt(id);
+        refreshFamilyInspector();
+    }
+    refreshLists();
+}
+const openButton = document.getElementById('open');
+const saveButton = document.getElementById('save');
+const clearButton = document.getElementById('clear');
+openButton.addEventListener('change', async (e) => {
+    openedFile = (await open(e, openedFile)) || openedFile;
+    refreshLists();
+});
+saveButton.onclick = () => download(openedFile);
+clearButton.onclick = () => {
+    openedFile = clear();
+    selectedPerson = null;
+    selectedFamily = null;
+    refreshLists();
+    refreshPersonInspector();
+    refreshFamilyInspector();
+};
+const peopleSection = document.querySelector('#people .list');
+const familiesSection = document.querySelector('#families .list');
+const personInspector = document.getElementById('person');
+const familyInspector = document.getElementById('family');
+const addPerson = document.getElementById('addPerson');
+addPerson.onclick = () => {
+    selectedPerson = AddPerson(openedFile);
+    refreshPersonInspector();
+    refreshLists();
+    let p = peopleSection.querySelector(`[data-id="${selectedPerson}"]`);
+    p?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+};
+const addFamily = document.getElementById('addFamily');
+addFamily.onclick = () => {
+    selectedFamily = AddFamily(openedFile);
+    refreshFamilyInspector();
+    refreshLists();
+    let f = familiesSection.querySelector(`[data-id="${selectedFamily}"]`);
+    f?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+};
+let openedFile = { people: [], families: [] };
+let selectedPerson = null;
+let sp = localStorage.getItem('selectedPerson');
+if (sp !== null && sp !== 'null')
+    selectedPerson = parseInt(sp);
+let selectedFamily = null;
+let sf = localStorage.getItem('selectedFamily');
+if (sf !== null && sf !== 'null')
+    selectedFamily = parseInt(sf);
+let fromLS = localStorage.getItem('openedFile');
+if (fromLS) {
+    openedFile = JSON.parse(fromLS) || { people: [], families: [] };
+}
+refreshLists();
+refreshPersonInspector();
+refreshFamilyInspector();
+//# sourceMappingURL=index.js.map
